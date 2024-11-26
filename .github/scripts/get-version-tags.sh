150809bf81594e3ec081db5b3e77db2bd368c9be
Lukasz Dziedziak <lukidzi@gmail.com>
2024-11-26 10:11:14 +0100

    fix tag retrieve
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- .github/scripts/get-version-tags.sh
+++ .github/scripts/get-version-tags.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 current_tag=$(git tag --list --merged origin/release --sort=-creatordate | head -n 1)
-main_tag=$(git tag --list --merged main --sort=-creatordate | head -n 1)
+main_tag=$(git tag --list --merged origin/main --sort=-creatordate | head -n 1)
 
 # Remove the prefix `v` and `-kong-*` suffix for comparison
 released_tag="${current_tag%-kong-*}"
